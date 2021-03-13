5efa994655b0342950a0bb0c584831a5df1e0fb1
rschio <rodrigoschio3@gmail.com>
2021-03-13 17:02:15 -0400

    fixes to models

--- storage/query.sql
+++ storage/query.sql
@@ -16,3 +16,71 @@ SELECT
 	GROUP BY service_id
 ;
 
+-- name: InsertRequest :execresult
+INSERT INTO requests (
+	method, uri, url, size,
+	header_accept, header_host,
+	header_user_agent
+) VALUES (
+	?,?,?,?,?,?,?
+);
+
+-- name: InsertResponse :execresult
+INSERT INTO responses (
+	status, size, content_length,
+	via, connection, access_control_allow_credentials,
+	access_control_allow_origin, content_type, server
+) VALUES (
+	?,?,?,?,?,?,?,?,?
+);
+
+-- name: InsertRoute :execresult
+INSERT INTO routes (
+	id,
+	created_at,
+	updated_at,
+	hosts,
+	methods,
+	preserve_host,
+	protocols,
+	regex_priority,
+	service_id,
+	strip_path
+) VALUES (
+	?,?,?,?,?,?,?,?,?,?
+);
+
+-- name: InsertService :execresult
+INSERT INTO services (
+	id,
+	created_at,
+	updated_at,
+	host,
+	name,
+	path,
+	port,
+	protocol,
+	read_timeout,
+	write_timeout,
+	connect_timeout,
+	retries
+) VALUES (
+	?,?,?,?,?,?,?,?,?,?,?,?
+);
+	
+-- name: InsertRecord :execresult
+INSERT INTO records (
+	consumer_id,
+	upstream_uri,
+	response_id,
+	request_id,
+	route_id,
+	service_id,
+	proxy_latency,
+	gateway_latency,
+	request_latency,
+	client_ip,
+	started_at
+) VALUES (
+	?,?,?,?,?,?,?,?,?,?,?
+);
--- storage/schema.sql
+++ storage/schema.sql
@@ -3,18 +3,18 @@ CREATE TABLE IF NOT EXISTS requests (
 	method varchar(255) NOT NULL,
 	uri varchar(255) NOT NULL,
 	url varchar(255) NOT NULL,
-	size varchar(255) NOT NULL,
-	header_accept varchar(255),
-	header_host varchar(255),
-	header_user_agent varchar(255),
+	size BIGINT NOT NULL,
+	header_accept varchar(255) NOT NULL,
+	header_host varchar(255) NOT NULL,
+	header_user_agent varchar(255) NOT NULL,
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS responses (
 	id BIGINT AUTO_INCREMENT NOT NULL,
 	status BIGINT NOT NULL,
-	size varchar(10) NOT NULL,
-	content_length varchar(10) NOT NULL,
+	size BIGINT NOT NULL,
+	content_length BIGINT NOT NULL,
 	via varchar(255) NOT NULL,
 	connection varchar(255) NOT NULL,
 	access_control_allow_credentials varchar(255) NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS routes (
 	preserve_host BOOLEAN NOT NULL,
 	protocols varchar(255) NOT NULL,
 	regex_priority BIGINT NOT NULL,
-	service_id BIGINT NOT NULL,
+	service_id varchar(255) NOT NULL,
 	strip_path BOOLEAN NOT NULL,
 	PRIMARY KEY (id)
 );
@@ -49,7 +49,8 @@ CREATE TABLE IF NOT EXISTS services (
 	protocol varchar(255) NOT NULL,
 	read_timeout BIGINT NOT NULL,
 	write_timeout BIGINT NOT NULL,
-	reties BIGINT NOT NULL,
+	connect_timeout BIGINT NOT NULL,
+	retries BIGINT NOT NULL,
 	PRIMARY KEY (id)	
 );
 
